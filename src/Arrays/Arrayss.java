ae4ff8c804db841992c1432ac775eb465a059cbf
Asalsmohammed <asala21mohammed7@gmail.com>
2025-11-25 23:13:16 +0300

    Arry_partone

---
+++ src/Arrays/Arrayss.java
@@ -0,0 +1,32 @@
+package Arrays;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Arrayss {
+    public void reverse_traversal(int[] a) {
+        for (int i = 0; i < a.length; i++)
+            System.out.println( "index["+i+"]="+a[i]);
+    }
+
+    public void traversal2(int[] a) {
+        for (int e : a)
+            System.out.println(e);
+    }
+
+    public void input(int[] a) {
+        System.out.println("please enter " + a.length + "element");
+
+        for (int i = 0; i < a.length; i++);
+
+
+    }
+    public void update(int[]a ,int index,int nvalue){
+        a[index]=nvalue;
+    }
+
+
+
+
+
+
+
+}
