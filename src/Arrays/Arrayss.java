f293e8db8adb86d5b85df2c57e4c05667f66db43
Asalsmohammed <asala21mohammed7@gmail.com>
2025-12-11 08:10:01 +0300

    Arryysp1 arryysp2

--- src/Arrays/Arrayss.java
+++ src/Arrays/Arrayss.java
@@ -2,9 +2,12 @@ package Arrays;
 import java.util.Scanner;
 import java.util.Arrays;
 public class Arrayss {
+
+
     public void reverse_traversal(int[] a) {
-        for (int i = 0; i < a.length; i++)
-            System.out.println( "index["+i+"]="+a[i]);
+        System.out.println("the elements are:");
+        for (int i = a.length - 1; i >= 0; i--)
+            System.out.println("index[" + i + "]=" + a[i]);
     }
 
     public void traversal2(int[] a) {
@@ -15,16 +18,50 @@ public class Arrayss {
     public void input(int[] a) {
         System.out.println("please enter " + a.length + "element");
 
-        for (int i = 0; i < a.length; i++);
+        for (int i = 0; i < a.length; i++) ;
 
 
     }
 
 
+    public void update(int[] a, int index, int nvalue) {
+        if (index < 0 || index >= a.length)
+            System.out.println(" index not found ");
+        else
+            a[index] = nvalue;
+    }
 
 
+    public void delete(int[] a, int index) {
+        if (index < 0 || index >= a.length)
+            System.out.println(" index not found ");
+        else
+            a[index] = 0;
+    }
+
+    public int linearsreach(int[] a, int svalue) {
+        for (int i=0;i<a.length;i++){
+            if (svalue == a[i])
+                return i;
+        }
+        return -1;
+    }
 
 
+        public int bainarySearch ( int[] a, int findex, int lindex, int svalue){
+            while (findex <= lindex) {
+                int mid = (findex + lindex) / 2;
+                System.out.println("mid" + mid);
+                if (svalue == a[mid])
+                    return mid;
+                if (svalue >= a[mid])
+                    findex = mid + 1;
+                if (svalue <= a[mid])
+                    findex = mid - 1;
 
 
-}
+            }
+            return -1;
+        }}
+
+
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -7,16 +7,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner a =new Scanner(System.in);
         Arrayss arr =new Arrayss();
-        //int []numbers=new int[5];
+
+            //int []numbers=new int[5];
         int []numbers={1,2,3,4,5};
-        arr.reverse_traversal(numbers);
-        //arr.traversal2(numbers);
+        //numbers[3]=7;
+      //  arr.reverse_traversal(numbers);
         //numbers[0]=8;
         // System.out.println(Arrays.toString(numbers));
-        System.out.println("please enter the"+numbers.length+" element");
-        numbers[0]= a.nextInt();
+        //System.out.println("please enter the"+numbers.length+" element");
+       // numbers[0]= a.nextInt();
+        //numbers[0]=1;
+//arr.traversal2(numbers);
+       // for(int i=0;i<numbers.length;i++)
+           // numbers[i]=a.nextInt();
+       // arr.update(numbers,7, 2) ;
+       // arr.delete(numbers,2) ;
+    // int result=arr.linearSreach(numbers,5);
+     int result =arr.bainarySearch(numbers,0,numbers.length-1,9);
+        if(result!=-1)
+            System.out.println( "value at"+result);
+        else
+            System.out.println("value not found");
 
-
+       // arr.traversal2(numbers);
+        //numbers[0]=a.nextInt();
+        int [] number= new int[5];
+        //System.out.println(arr.toString(numbers));
+       // int [] numbe ={1,2,3,4,5};
+       // for (int i=0;i<numbe.length;i++);
 
 
 
